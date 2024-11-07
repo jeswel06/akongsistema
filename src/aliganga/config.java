844b6c92762e41b92ee3a74b9a804a427d2bd2f6
19Drey <andreestrera20@gmail.com>
2024-11-07 09:02:22 -0800

    updated sql

--- src/aliganga/config.java
+++ src/aliganga/config.java
@@ -15,7 +15,7 @@ public class config {
         Connection con = null;
         try {
             Class.forName("org.sqlite.JDBC"); // Load the SQLite JDBC driver
-            con = DriverManager.getConnection("jdbc:sqlite:aliganga"); // Establish connection
+            con = DriverManager.getConnection("jdbc:sqlite:aliganga.db"); // Establish connection
             System.out.println("Connection Successful");
         } catch (Exception e) {
             System.out.println("Connection Failed: " + e);
